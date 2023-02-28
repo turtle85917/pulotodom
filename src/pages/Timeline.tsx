67c8cd8be68e6c9fc5f30ba84ed5f59ffd455027
turtle85917 <siu85917@gmail.com>
2023-02-28 16:52:05 +0900

    Use mdx

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
 import HttpStatusPage from "@components/HttpStatusPage";
+import { MDXProvider } from "@mdx-js/react";
+import TestItem from "@data/timeline/20200219.mdx";
 
 export default function Timeline(): JSX.Element {
-  return <HttpStatusPage statusCode="501" />;
+  if (process.env.NODE_ENV === "production") return <HttpStatusPage statusCode="501" />
+
+  return <Container>
+    <MDXProvider
+      components={{
+        h1: MdxContentHeader
+      }}
+    >
+      <TestItem />
+    </MDXProvider>
+  </Container>;
 }
+
+const Container = styled.article`
+  margin-top: calc(var(--header-height) + 5px);
+`;
+
+const MdxContentHeader: React.FC = (props) => <StyledMdxContentHeader {...props} />
+const StyledMdxContentHeader = styled.h1`
+  font-size: 15pt;
+  font-family: Desc;
+`;
